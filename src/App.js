992f6e452767813935c73004a407ae472abd5baf
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2020-09-27 21:15:06 -0300

    navigation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import SignUp from './containers/SignUp';
+import SignIn from './containers/SignIn';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      ola
+    </NavigationContainer>
+  );
+}
