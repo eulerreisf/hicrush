f2f29f553aa49aeddae40d1a4a2e602219a531c7
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2020-10-04 22:52:17 -0300

    navigations

--- src/containers/SignIn/SignIn.js
+++ src/containers/SignIn/SignIn.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-const SignIn = () => (
+const SignIn = ({ navigation }) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text>SignIn</Text>
+    <Button
+      title="Ir para o Cadastro"
+      onPress={() => navigation.navigate('SignUp')}
+    />
   </View>
 );
 
