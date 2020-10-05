2fd2871cc87ceeec70411f53c7ec3725a9082ab1
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2020-10-04 22:32:23 -0300

    navigator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import SignUp from './containers/SignUp';
 import SignIn from './containers/SignIn';
 
-export default function App() {
-  return <SignUp />;
-}
+
+
+const Stack = createStackNavigator();
+
+const App = () =>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+  </NavigationContainer>;
+
+
+export default App;
