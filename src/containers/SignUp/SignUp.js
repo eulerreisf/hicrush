1a522b90b758a770d4b4ac1427a7b9d06f2395b9
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2021-08-07 10:45:19 -0300

    new

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { View, Text, Button } from 'react-native';
 
-const SignUp = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>SignUp</Text>
-    <Button
-      title="Ir para o Login"
-      onPress={() => navigation.navigate('SignIn')}
-    />
-  </View>
-);
+const SignUp = ({ navigation }) => {
+  const { control, handleSubmit } = useForm();
+
+
+  const onSubmit = data => console.log('OnSubmit data =>', data);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>SignUp</Text>
+      <Button
+        title="Ir para o Login"
+        onPress={() => navigation.navigate('SignIn')}
+      />
+    </View>
+  );
+};
 
 export default SignUp;
